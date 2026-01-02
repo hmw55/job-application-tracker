8f0a9ab266fbb40c0f3f8812fbbf427c4fae033b
hmw55 <hollandwesley55@gmail.com>
2026-01-02 13:36:22 -0700

    Initialize SQLite database and create Company table

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./job_tacker.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./job_tracker.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 
+from .database import engine
+from . import models
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 @app.get("/")
